d85741ca3393a338597dc3112e725a558ca6a21e
minseong1231 <minseonglove@estsoft.com>
2023-06-02 19:17:57 +0900

    FEAT : 다크 모드 고정

--- app/src/main/java/com/weit/odya/HiltApplication.kt
+++ app/src/main/java/com/weit/odya/HiltApplication.kt
@@ -1,6 +1,7 @@
 package com.weit.odya
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
@@ -10,5 +11,6 @@ class HiltApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Logger.addLogAdapter(AndroidLogAdapter())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }

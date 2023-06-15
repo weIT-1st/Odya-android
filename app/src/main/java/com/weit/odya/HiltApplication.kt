1fc75c348024ed7ba64731c92150822578ea6d9e
minseonglove <qnfpdlr124@naver.com>
2023-06-16 01:13:13 +0900

    FEAT : ContextProvider 추가

--- app/src/main/java/com/weit/odya/HiltApplication.kt
+++ app/src/main/java/com/weit/odya/HiltApplication.kt
@@ -4,13 +4,20 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
+import com.weit.data.util.ContextProvider
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class HiltApplication : Application() {
+
+    @Inject
+    lateinit var contextProvider: ContextProvider
+
     override fun onCreate() {
         super.onCreate()
         Logger.addLogAdapter(AndroidLogAdapter())
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        contextProvider.start(this)
     }
 }

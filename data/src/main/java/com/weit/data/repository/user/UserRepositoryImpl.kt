81696b514b7feb40e7c2e75adf6722f3c0658132
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-06-29 23:40:54 +0900

    feat : 정규표현식을 이용해 이메일과 전화번호 양식 검정 기능 추가

--- data/src/main/java/com/weit/data/repository/user/UserRepositoryImpl.kt
+++ data/src/main/java/com/weit/data/repository/user/UserRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.weit.data.repository.user
 
+import android.util.Patterns
 import com.weit.data.model.user.UserDTO
 import com.weit.data.source.UserDataSource
+import com.weit.domain.model.exception.RegexException
 import com.weit.domain.model.user.User
 import com.weit.domain.repository.user.UserRepository
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
@@ -17,15 +20,29 @@ class UserRepositoryImpl @Inject constructor(
     }
 
     override suspend fun updateEmail(email: String): Result<Unit> {
-        return runCatching {
-            userDataSource.updateEmail(email)
+
+        val result : Result<Unit> = runCatching{
+
+            if(Pattern.matches("^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", email)){
+                userDataSource.updateEmail(email)
+            } else {
+                throw RegexException()
+            }
         }
+
+        return result
     }
 
     override suspend fun updatePhoneNumber(phonenumber: String): Result<Unit> {
-        return runCatching {
-            userDataSource.updatePhoneNumber(phonenumber)
+        val result : Result<Unit> = runCatching{
+
+            if (Pattern.matches("^01(?:0|1|[6-9]) - (?:\\d{3}|\\d{4}) - \\d{4}$", "010-0000-0000")) {
+                userDataSource.updatePhoneNumber(phonenumber)
+            } else {
+                throw RegexException()
+            }
         }
+        return result
     }
 
     override suspend fun updateInformation(nickname: String): Result<Unit> {
--- domain/src/main/java/com/weit/domain/usecase/user/UpdateUserEmailUseCase.kt
+++ domain/src/main/java/com/weit/domain/usecase/user/UpdateUserEmailUseCase.kt
@@ -3,7 +3,7 @@ package com.weit.domain.usecase.user
 import com.weit.domain.repository.user.UserRepository
 import javax.inject.Inject
 
-class UpdateUserEamilUseCase@Inject constructor(
+class UpdateUserEmailUseCase@Inject constructor(
     private val userRepository: UserRepository
 ) {
     suspend fun updateEmail(newEmail : String) : Result<Unit> =

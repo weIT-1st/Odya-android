52312d754b3419c50068d840b328ae603a051059
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-06-30 18:55:38 +0900

    feat : splash화면 1초동안 유지

--- presentation/src/main/java/com/weit/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/weit/presentation/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.weit.presentation.ui
 
+import android.os.Bundle
 import android.view.View
+import android.view.ViewTreeObserver
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.weit.presentation.databinding.ActivityMainBinding
 import com.weit.presentation.ui.base.BaseActivity
@@ -8,8 +11,25 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
-    override fun preLoad(){
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun preLoad() {
         installSplashScreen()
+        viewModel.splashDelay()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (viewModel.splashReady) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
     }
-    val content: View = findViewById(android.R.id.content)
 }

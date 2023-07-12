b6b00caf0eba7327a67aeaa45548a92fa18a446d
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-07-13 01:30:04 +0900

    DESIGN : 간단한 수정들
    1. layout 명을 fragment~ 로 수정
    2. tab id 추가
    3. android:text="홍길동" -> tools:text"홍길동"
    4. text style 일관 변경 (피그마 보고 수정)
    5. tv_name (홍길동) 최대 글자수 10으로 설정
    	-> 닉네임 최대 글자수 관련 정보가 없어서 임의로 설정
    6. editText의 textColor 설정
    	-> 피그마에 나와 있는 색 #595959가 지정된 이름이 없어서 색을 바로 적용
    7. 프사 크기 고정
    8. 홍길동 옆 다이아몬드 홍길동과 높이 맞추기

--- presentation/src/main/java/com/weit/presentation/ui/myinfo/MyinfoFriendActivity.kt
+++ presentation/src/main/java/com/weit/presentation/ui/myinfo/MyinfoFriendActivity.kt
@@ -2,13 +2,12 @@ package com.weit.presentation.ui.myinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.tabs.TabLayout
 import com.weit.presentation.R
 
 class MyinfoFriendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_myinfo_friend)
+        setContentView(R.layout.fragment_myinfo_friend)
 
     }
 }

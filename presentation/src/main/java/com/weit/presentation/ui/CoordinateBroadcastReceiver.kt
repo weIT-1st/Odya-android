163d38f509f4cd6ff2740c28f0260678f0eb9682
sia7997 <sia7997@naver.com>
2023-07-29 17:08:44 +0900

    CHORE : manifest추가, 채널명 변경, 불필요한 코드 제거

--- presentation/src/main/java/com/weit/presentation/ui/CoordinateBroadcastReceiver.kt
+++ presentation/src/main/java/com/weit/presentation/ui/CoordinateBroadcastReceiver.kt
@@ -11,14 +11,6 @@ class CoordinateBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
 
         if(intent.action==Intent.ACTION_BOOT_COMPLETED){
-            StringBuilder().apply {
-                append("Action: ${intent.action}\n")
-                append("URI: ${intent.toUri(Intent.URI_INTENT_SCHEME)}\n")
-                toString().also { log ->
-                    Log.d("MyBroadcastReceiver", log)
-                    Toast.makeText(context, log, Toast.LENGTH_LONG).show()
-                }
-            }
             val serviceIntent = Intent(context, CoordinateForegroundService::class.java)
             context.startForegroundService(serviceIntent)
         }
--- presentation/src/main/java/com/weit/presentation/ui/CoordinateForegroundService.kt
+++ presentation/src/main/java/com/weit/presentation/ui/CoordinateForegroundService.kt
@@ -11,6 +11,7 @@ import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.weit.presentation.R
 
@@ -18,12 +19,13 @@ class CoordinateForegroundService : Service() {
 
     companion object {
         private const val NOTIFICATION_ID = 123
-        private const val CHANNEL_ID = "ForegroundServiceChannel"
+        private const val CHANNEL_ID = "CoordinateChannel"
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         createNotificationChannel()
+
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(
             this,
@@ -31,8 +33,8 @@ class CoordinateForegroundService : Service() {
         )
 
         val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Foreground Service")
-            .setContentText("포그라운드 서비스가 실행 중입니다.")
+            .setContentTitle("Odya")
+            .setContentText("좌표 수집 중입니다.")
             .setSmallIcon(R.drawable.ic_splash_logo)
             .setContentIntent(pendingIntent)
             .build()
@@ -47,14 +49,12 @@ class CoordinateForegroundService : Service() {
     }
 
     private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_ID,
-                "Foreground Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            manager.createNotificationChannel(channel)
-        }
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            "Odya Coordinate Channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
     }
 }

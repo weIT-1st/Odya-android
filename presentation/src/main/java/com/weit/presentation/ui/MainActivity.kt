c15737cfb7f9374794a13f756522e409b3075399
sia7997 <sia7997@naver.com>
2023-07-30 22:49:13 +0900

    CHORE : 앱이 꺼졌을 때 포그라운드 서비스 실행 코드추가

--- presentation/src/main/java/com/weit/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/weit/presentation/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.weit.presentation.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -11,7 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-    val br: CoordinateBroadcastReceiver = CoordinateBroadcastReceiver()
+
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +25,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             .findFragmentById(R.id.navigation_main) as NavHostFragment? ?: return
         navController = host.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val serviceIntent = Intent(this, CoordinateForegroundService::class.java)
+        startForegroundService(serviceIntent)
     }
 }

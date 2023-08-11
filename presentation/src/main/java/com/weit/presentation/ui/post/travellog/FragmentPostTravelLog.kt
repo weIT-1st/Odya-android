7086b6273289694737a05d332f2a004f535edf09
minseonglove <qnfpdlr124@naver.com>
2023-08-11 13:26:30 +0900

    ODYA-107 : 여행일지 작성 view 추가

---
+++ presentation/src/main/java/com/weit/presentation/ui/post/travellog/FragmentPostTravelLog.kt
@@ -0,0 +1,11 @@
+package com.weit.presentation.ui.post.travellog
+
+import com.weit.presentation.databinding.FragmentPostTravelLogBinding
+import com.weit.presentation.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FragmentPostTravelLog : BaseFragment<FragmentPostTravelLogBinding>(
+    FragmentPostTravelLogBinding::inflate
+){
+}

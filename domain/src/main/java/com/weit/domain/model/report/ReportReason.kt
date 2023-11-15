954944210eb03aa21e7c86528342a0dc7b5ccd40
LeEjoNgmIn1 <115535048+LeEjoNgmIn1@users.noreply.github.com>
2023-11-15 16:09:48 +0900

    DESIGN : 신고하기 XML완성

--- domain/src/main/java/com/weit/domain/model/report/ReportReason.kt
+++ domain/src/main/java/com/weit/domain/model/report/ReportReason.kt
@@ -5,6 +5,6 @@ enum class ReportReason {
     PORNOGRAPHY, // 음란성이 포함된 글
     SWEAR_WORD, // 욕설 / 생명 경시 / 혐오 / 차별적인 글
     OVER_POST, // 개시글 도배
-    INFO_LEAK, // 개인정보 노출 및 불버 정보
+    INFO_LEAK, // 개인정보 노출 및 불법 정보
     OTHER, // 기타
 }

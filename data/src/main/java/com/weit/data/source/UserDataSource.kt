54f6eaa52c5ea5900b654af7023792c8f4f8b2a3
sia7997 <sia7997@naver.com>
2023-12-12 22:25:32 +0900

    Chore : 리뷰반영

--- data/src/main/java/com/weit/data/source/UserDataSource.kt
+++ data/src/main/java/com/weit/data/source/UserDataSource.kt
@@ -1,12 +1,6 @@
 package com.weit.data.source
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.stringSetPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.weit.data.model.ListResponse
 import com.weit.data.model.user.SearchUserContentDTO
 import com.weit.data.model.user.UserDTO
@@ -14,12 +8,8 @@ import com.weit.data.service.UserService
 import com.weit.domain.model.user.SearchUserRequestInfo
 import com.weit.domain.model.user.User
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
 import okhttp3.MultipartBody
 import retrofit2.Response
-import java.io.IOException
 import javax.inject.Inject
 
 class UserDataSource @Inject constructor(

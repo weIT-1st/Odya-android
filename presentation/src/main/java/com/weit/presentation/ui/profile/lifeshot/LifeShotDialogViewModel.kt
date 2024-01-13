2f3184e81f39a8cf0ed39e57f4c4ed44bcac1101
sia7997 <sia7997@naver.com>
2024-01-13 22:22:31 +0900

    Chore : 파일 수정

--- presentation/src/main/java/com/weit/presentation/ui/profile/lifeshot/LifeShotDialogViewModel.kt
+++ presentation/src/main/java/com/weit/presentation/ui/profile/lifeshot/LifeShotDialogViewModel.kt
@@ -3,32 +3,16 @@ package com.weit.presentation.ui.profile.lifeshot
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.google.android.gms.maps.model.LatLng
-import com.orhanobut.logger.Logger
-import com.weit.domain.model.image.UserImageResponseInfo
-import com.weit.domain.model.place.PlacePrediction
-import com.weit.domain.usecase.community.comment.DeleteCommentsUseCase
-import com.weit.domain.usecase.community.comment.GetCommentsUseCase
-import com.weit.domain.usecase.community.comment.RegisterCommentsUseCase
-import com.weit.domain.usecase.community.comment.UpdateCommentsUseCase
 import com.weit.domain.usecase.image.SetLifeShotUseCase
-import com.weit.domain.usecase.user.GetUserUseCase
-import com.weit.presentation.model.post.travellog.TravelPeriod
 import com.weit.presentation.model.profile.lifeshot.SelectLifeShotImageDTO
-import com.weit.presentation.ui.feed.detail.CommentDialogViewModel
-import com.weit.presentation.ui.login.inputuserinfo.LoginInputUserInfoViewModel
-import com.weit.presentation.ui.post.selectplace.SelectPlaceViewModel
 import com.weit.presentation.ui.util.MutableEventFlow
 import com.weit.presentation.ui.util.asEventFlow
-import com.weit.presentation.ui.util.toMillis
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.time.LocalDate
-import java.util.Calendar
 
 class LifeShotDialogViewModel @AssistedInject constructor(
     private val setLifeShotUseCase: SetLifeShotUseCase,
